b2f550cab33de03f303de12afe1c03c28c5fa70b
Damian Śleziona <damian.sleziona@gmail.com>
2018-02-17 16:00:03 +0100

    Add some stuff

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,8 @@
+class StaticPagesController < ApplicationController
+
+	def contact; end
+
+	def about; end
+
+	def mission; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   get '/test', to: proc { |e| [200, {}, ["ok"]] }
 
   get '/users', to: "users#index"
-  get '/contact', to: "contact#index"
+  get '/contact', to: "static_pages#contact"
+  get '/about', to: "static_pages#about"
+  get '/mission', to: "static_pages#mission"
+
 end
